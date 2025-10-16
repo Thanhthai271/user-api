fead624ef58b3585c7ac4c2c497a198e381bc782
Thanhthai271 <thanhthai2712002@gmail.com>
2025-10-16 17:07:37 +0700

    new update

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {  
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/user_api");
+        await mongoose.connect("mongodb://127.0.0.1:27017/user");
         console.log("MongoDB Connected");
     } catch (err) {
         console.error("MongoDB connection failed", err);
