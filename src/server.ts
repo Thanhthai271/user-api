b5a3f727e5ba0e5000791cc36c18d5a39a330b75
Thanhthai271 <thanhthai2712002@gmail.com>
2026-01-06 09:24:28 +0700

    test

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,8 @@ import dotenv from "dotenv"
 dotenv.config();
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+export default app;
