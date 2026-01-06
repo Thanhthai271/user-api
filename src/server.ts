7e7bb203306325d5b3470de7be32c559e9ce893c
Thanhthai271 <thanhthai2712002@gmail.com>
2026-01-06 09:31:33 +0700

    newdeploy

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,9 @@ import app from "./app"
 import dotenv from "dotenv"
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000 ;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 if (process.env.NODE_ENV !== 'production') {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
-export default app;
+export default app; 
