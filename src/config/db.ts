773696170a0b6c3698c4ca15c71610e18ef3e16f
Thanhthai271 <thanhthai2712002@gmail.com>
2026-01-06 11:34:49 +0700

    testDb

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,27 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv"
 
-dotenv.config()
-export const connectDB = async () => {  
+// dotenv.config()
+// export const connectDB = async () => {  
+//     try {
+//         await mongoose.connect(process.env.DB_URL as string);
+//         console.log("MongoDB Connected");
+//     } catch (err) {
+//         console.error("MongoDB connection failed", err);
+//         process.exit(1);
+//     }
+// };
+
+export const connectDB = async () => {   
+    const url = process.env.DB_URL;
+    if (!url || !url.startsWith("mongodb")) {
+        console.error("URI sai định dạng:", url);
+        return;
+    }
     try {
-        await mongoose.connect(process.env.DB_URL as string);
-        console.log("MongoDB Connected");
-    } catch (err) {
-        console.error("MongoDB connection failed", err);
-        process.exit(1);
+        await mongoose.connect(url);
+        console.log("MongoDB Connected Successfully");
+    } catch (err : any ) {
+        console.error("Lỗi kết nối thực tế:", err.message);
     }
 };
