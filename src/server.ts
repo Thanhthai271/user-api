a66fca225734aba8af1339576ef495a6ee457826
Thanhthai271 <thanhthai2712002@gmail.com>
2026-01-06 11:21:58 +0700

    testlog

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,10 @@ dotenv.config();
 const PORT = process.env.PORT || 5000 ;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 if (process.env.NODE_ENV !== 'production') {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
 }
-export default app; 
+
+console.log("DB_URL check:", process.env.DB_URL ? "Đã nhận" : "Chưa nhận");
+export default app;     
