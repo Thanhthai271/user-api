e196df468e0bb4171ece6d3cb90687a5e4274e0c
Thanhthai271 <thanhthai2712002@gmail.com>
2026-01-07 16:06:22 +0700

    fixxlogin

--- src/config/db.ts
+++ src/config/db.ts
@@ -15,7 +15,7 @@ import dotenv from "dotenv"
 
 
 // Tắt buffering: Nếu không kết nối được sẽ báo lỗi ngay, không đợi (giúp tránh lỗi 500 của Vercel)
-mongoose.set('bufferCommands', false); 
+mongoose.set('bufferCommands', false);
 
 export const connectDB = async () => {
     // Kiểm tra nếu đã kết nối rồi thì không kết nối lại
@@ -24,11 +24,11 @@ export const connectDB = async () => {
     try {
         await mongoose.connect(process.env.DB_URL as string, {
             // Chỉ đợi tối đa 5 giây để tìm server DB
-            serverSelectionTimeoutMS: 5000, 
+            serverSelectionTimeoutMS: 5000,
         });
         console.log("MongoDB đã kết nối thành công");
-    } catch (error) {
-        console.error("Lỗi kết nối MongoDB:", error);
-        // Không throw lỗi ở đây để tránh crash app, hoặc xử lý tùy logic của bạn
+    } catch (error: any) {
+        console.error("LỖI KẾT NỐI DB CHI TIẾT:", error.message);
+        // In ra mã lỗi để biết là sai pass (AuthFailed) hay sai địa chỉ
     }
 };
